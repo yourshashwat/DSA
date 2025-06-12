d0bd1994242731a0ac4fa8f1fca6dc203ca9c42a
yourshashwat <84892195+yourshashwat@users.noreply.github.com>
2025-06-13 01:22:42 +0530

    Update solution.cpp

--- painters-partition-problem/solution.cpp
+++ painters-partition-problem/solution.cpp
@@ -1,1 +1,33 @@
+int countPainters(vector <int> &boards, int mid){
+    int painted=0, painters=1;
+    for(auto it:boards){
+        if(it+painted<=mid){
+            painted+=it;
+        }
+        else{
+            painted=it;
+            painters++;
+        }
+    }
+    return painters;
+}
 
+int findLargestMinDistance(vector<int> &boards, int k)
+{
+    //    Write your code here.
+    int n= boards.size();
+    int low= *max_element(boards.begin(), boards.end());
+    int high= accumulate(boards.begin(), boards.end(),0);
+    if(k==1) return high;
+    if(k==n) return low;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(countPainters(boards,mid)<=k){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return low;
+}
