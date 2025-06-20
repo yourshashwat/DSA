ee400be20324ff92e8231ca8e495e393eb10bf40
yourshashwat <84892195+yourshashwat@users.noreply.github.com>
2025-06-20 19:25:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (56.56%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        set<char> st={'.','+','-','0', '1', '2', '3', '4', '5', '6', '7', '8', '9',' '};
-        long long ans=0;
-        char sign='+';
-        int signCount=0, digitCount=0;
-        if(s[0]=='.') return 0;
-        for(int i=0;i<s.size(); i++){
-            if(st.find(s[i])==st.end()) break;
-
-            if(s[i]-48>=0 && s[i]-48<=9){
-                digitCount++;
-                ans= ans*10+ (s[i]-48);
-            }
-            if(s[i]==' ' && digitCount>=1) break;
-            if(s[i]==' ' && signCount>=1) break;
-            if(s[i]==' '&& ans==0) continue;
-            
-            if(s[i]=='+' || s[i]== '-' ){
-                if(digitCount>=1) break;
-                sign=s[i];
-                signCount++;
-                if(signCount>1) break;
-            }
-            if(s[i]=='.') break;
-            if(ans>=INT_MAX && sign=='+') return INT_MAX;
-            if(ans>=abs((long long)INT_MIN) && sign=='-') return INT_MIN;
-            
-            
+          int num = 0, i = 0, sign = 1;
+       while (s[i] == ' ')
+       {
+        ++i;
+       } 
+       if (i < s.size() && (s[i] == '+' || s[i] == '-'))
+       {
+        sign = s[i] == '+' ? 1 : -1;
+        ++i;
+       }
+       while (i < s.size() && isdigit(s[i]))
+       {
+        if (num > INT_MAX/10 || (num == INT_MAX/10 && s[i] > '7'))
+        {
+            return sign == -1 ? INT_MIN : INT_MAX;
         }
-        return sign=='-'?-(ans):ans; 
+        num = num*10 + (s[i] - '0');
+        ++i;
+       }
+
+       return num*sign;
+
     }
 };
