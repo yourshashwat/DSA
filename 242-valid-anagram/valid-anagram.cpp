45faf239ace2dde2473ae823a49416ec8a8cf11a
yourshashwat <84892195+yourshashwat@users.noreply.github.com>
2025-06-23 15:50:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (74.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,11 +4,14 @@ public:
         int m=s.size();
         int n= t.size();
         if(m!=n) return false;
-        unordered_map<char,int> mp1,mp2;
+        int freq[26]={0};
         for(int i=0;i<s.size(); i++){
-            mp1[s[i]]++;
-            mp2[t[i]]++;
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
         }
-        return mp1==mp2;
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0) return false;
+        }
+        return true;
     }
 };
