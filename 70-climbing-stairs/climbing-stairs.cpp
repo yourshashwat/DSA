e3c8bbb0e280c9be566653a17de40950ba5c4550
yourshashwat <84892195+yourshashwat@users.noreply.github.com>
2025-06-28 02:08:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.44%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
-    int t[46];
-    int solve(int n){
-        if(n==0) return 1;
-        if(n<0) return 0;
-        if(t[n]!=-1) return t[n];
-        int one_step=solve(n-1);
-        int two_step= solve(n-2);
 
-        return t[n]= one_step+two_step;
+  int climbStairs(int n) {
+        if(n==1|| n==2) return n;
+       int a=1;
+       int b=2;
+       int c;
+       for(int i=3; i<=n; i++){
+        c=a+b;
+        int temp=b;
+        b=c;
+        a=temp;
+       }
+    return c;
     }
-    int climbStairs(int n) {
-        memset(t,-1, sizeof(t));
-        return solve(n);
-    }
+    // int t[46];
+    // int solve(int n){
+    //     if(n==0) return 1;
+    //     if(n<0) return 0;
+    //     if(t[n]!=-1) return t[n];
+    //     int one_step=solve(n-1);
+    //     int two_step= solve(n-2);
+
+    //     return t[n]= one_step+two_step;
+    // }
+    // int climbStairs(int n) {
+    //     memset(t,-1, sizeof(t));
+    //     return solve(n);
+    // }
 };
