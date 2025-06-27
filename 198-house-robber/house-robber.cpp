a798138a7e1e74623fb12ffe007d5809eb303b9d
yourshashwat <84892195+yourshashwat@users.noreply.github.com>
2025-06-28 02:56:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (63.82%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
-int t[101];
-    int solve(vector<int>& nums, int i, int n){
-        if(i>=n) return 0;
-        if(t[i]!=-1) return t[i];
-        int steal= nums[i]+ solve(nums, i+2, n);
-        int skip= solve(nums, i+1, n);
 
-        return t[i]= max(steal, skip);
-    }
     int rob(vector<int>& nums) {
         int n= nums.size();
-        memset(t,-1,sizeof(t));
-        return solve(nums,0,n);
+         if(n==1) return nums[0];
+         vector<int> t(n+1);
+         t[0]=0;
+         t[1]=nums[0];
+         for(int i=2;i<=n;i++){
+            int steal= nums[i-1]+ t[i-2];
+            int skip= t[i-1];
+             t[i]=max(steal, skip);
+         }
+         return t[n];
     }
+// int t[101];
+//     int solve(vector<int>& nums, int i, int n){
+//         if(i>=n) return 0;
+//         if(t[i]!=-1) return t[i];
+//         int steal= nums[i]+ solve(nums, i+2, n);
+//         int skip= solve(nums, i+1, n);
+
+//         return t[i]= max(steal, skip);
+//     }
+//     int rob(vector<int>& nums) {
+//         int n= nums.size();
+//         memset(t,-1,sizeof(t));
+//         return solve(nums,0,n);
+//     }
 };
