e750bc1b00d7c7bd816af1afd029d64f765848fd
yourshashwat <84892195+yourshashwat@users.noreply.github.com>
2025-07-08 00:03:40 +0530

    Time: 64 ms (47.92%) | Memory: 75 MB (39.30%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
-        sort(events.begin(), events.end());
-    priority_queue<int, vector<int>, greater<int>> minHeap;
-    int day = 0, i = 0, n = events.size(), count = 0;
+       int n=events.size();
+       sort(events.begin(), events.end());
+       priority_queue<int, vector<int>, greater<int>> pq;
+       int count=0, i=0, day=events[0][0];
+        
+        while(i<n || !pq.empty()){
+            if(pq.empty()){
+                day=events[i][0];
+            }
 
-    while (i < n || !minHeap.empty()) {
-        if (minHeap.empty()) {
-            day = events[i][0];
-        }
-        while (i < n && events[i][0] <= day) {
-            minHeap.push(events[i][1]);
-            i++;
-        }
-        while (!minHeap.empty() && minHeap.top() < day) {
-            minHeap.pop();
-        }
-        if (!minHeap.empty()) {
-            minHeap.pop();
-            count++;
-        }
-        day++;
-    }
+            while(i<n && events[i][0]<=day){
+                pq.push(events[i][1]);
+                i++;
+            }
+ 
+            while(!pq.empty() && pq.top()<day){
+                pq.pop();
+            }
 
-    return count;
+            if(!pq.empty()){
+                pq.pop();
+                count++;
+            }
+            day++;
 
+
+        }
+        return count;
         
     }
 };
