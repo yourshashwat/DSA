71215fb3bfc9142981af1ea2fabdd462498dd5d7
yourshashwat <84892195+yourshashwat@users.noreply.github.com>
2025-08-16 17:54:50 +0530

    Time: 5 ms (58.99%) | Memory: 20 MB (48.39%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+bool topoSort(unordered_map<int, vector<int>> &adj, int n, vector<int> &indegree) {
+         queue<int> q;
+         int count=0;
+         for(int i=0; i<n; i++){
+    if(indegree[i] == 0){
+        q.push(i);
+        count++;
+    }
+}
+         while(!q.empty()){
+            int u= q.front();
+            q.pop();
+            for(int &v: adj[u]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    q.push(v);
+                    count++;
+                }
+            }
+         }
+         if(count==n) return true;
+         else return false;
+
+    }
+
+
+    bool canFinish(int numCourses, vector<vector<int>>& prereq) {
+        unordered_map<int, vector<int>> adj;
+        vector<int> indegree(numCourses);
+
+        for(auto &v: prereq){
+            int a=v[0];
+            int b= v[1];
+            adj[b].push_back(a);
+            indegree[a]++;
+        }
+
+        return topoSort(adj, numCourses, indegree);
+    }
+};
