e7e02c1726a73bad1af9b16ee79a925ce39b6bf4
Shubham Shashwat <84892195+yourshashwat@users.noreply.github.com>
2026-05-24 15:49:52 +0530

    Time: 477 ms (10.00%) | Memory: 53.2 MB (14.11%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        int n= nums.size();
-        vector<bool> t(n, false);
-        t[0]= true;
-        for(int i=1; i<n; i++){
-            for(int j=i-1; j>=0; j--){
-                if(t[j]== true && nums[j]+j>=i){
-                    t[i]= true;
-                    break;
-                }
-            }
+    // bool canJump(vector<int>& nums) {
+    //     int n= nums.size();
+    //     vector<bool> t(n, false);
+    //     t[0]= true;
+    //     for(int i=1; i<n; i++){
+    //         for(int j=i-1; j>=0; j--){
+    //             if(t[j]== true && nums[j]+j>=i){
+    //                 t[i]= true;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return t[n-1];
+    // }
+    int t[10001];
+    
+    bool solve(vector<int> &nums, int n, int idx){
+        if(idx==n-1) return true;
+        if(t[idx]!=-1) return t[idx];
+
+        for(int i=1; i<=nums[idx]; i++){
+            if(solve(nums, n, idx+i)== true) return t[idx]= true;
         }
-        return t[n-1];
+
+        return t[idx]= false;
+
+    }
+
+    bool canJump(vector<int>& nums){
+        int n=nums.size();
+        memset(t, -1, sizeof(t));
+        return solve(nums,n,0);
     }
 };
