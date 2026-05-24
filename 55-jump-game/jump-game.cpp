ec1728abc6e32c6c196c91ee38242562569cda32
Shubham Shashwat <84892195+yourshashwat@users.noreply.github.com>
2026-05-24 15:59:55 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (56.74%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -14,23 +14,36 @@ public:
     //     }
     //     return t[n-1];
     // }
-    int t[10001];
+
+
+    // int t[10001];
     
-    bool solve(vector<int> &nums, int n, int idx){
-        if(idx==n-1) return true;
-        if(t[idx]!=-1) return t[idx];
+    // bool solve(vector<int> &nums, int n, int idx){
+    //     if(idx==n-1) return true;
+    //     if(t[idx]!=-1) return t[idx];
 
-        for(int i=1; i<=nums[idx]; i++){
-            if(solve(nums, n, idx+i)== true) return t[idx]= true;
+    //     for(int i=1; i<=nums[idx]; i++){
+    //         if(solve(nums, n, idx+i)== true) return t[idx]= true;
+    //     }
+
+    //     return t[idx]= false;
+
+    // }
+
+    // bool canJump(vector<int>& nums){
+    //     int n=nums.size();
+    //     memset(t, -1, sizeof(t));
+    //     return solve(nums,n,0);
+    // }
+
+    bool canJump(vector<int> &nums){
+        int n= nums.size();
+        int maxReachable=0;
+        for(int i=0; i<n; i++){
+            if(i>maxReachable) return false;
+            maxReachable= max(maxReachable, i+nums[i]);
+            if(maxReachable>=n-1) return true;
         }
-
-        return t[idx]= false;
-
-    }
-
-    bool canJump(vector<int>& nums){
-        int n=nums.size();
-        memset(t, -1, sizeof(t));
-        return solve(nums,n,0);
+        return false;
     }
 };
